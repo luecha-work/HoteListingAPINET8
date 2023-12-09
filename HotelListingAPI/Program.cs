54c481a19a3746c7396dc5f24e59ff1e501a978a
luecha.kanm <luecha.kanm@gmail.com>
2023-12-09 16:14:34 +0700

    add cors and add log Serilog

---
+++ HotelListingAPI/Program.cs
@@ -0,0 +1,45 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//TODO: Add cors url1
+builder
+    .Services
+    .AddCors(options =>
+    {
+        options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+    });
+
+//TODO: Add Serilog1
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//TODO: Add Serilog2
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+//TODO: Add cors url2
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
